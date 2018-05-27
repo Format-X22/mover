5df61727eaf292dbe8ee109e2ea7fd0cfe5f2b73
Oleg <oleg.pav.m@gmail.com>
2018-05-27 21:59:21 +0300

    Абстрактная биржа.

--- stock/AbstractStock.rb
+++ stock/AbstractStock.rb
@@ -5,4 +5,43 @@ class AbstractStock
 		@stock_id = stock_id
 		@keys = File.read('keys.txt').split("\n")
 	end
+
+	def make_enter(direction, price, amount)
+		# abstract
+	end
+
+	def make_take(direction, price, amount)
+		# abstract
+	end
+
+	def make_stop(direction, price, amount)
+		# abstract
+	end
+
+	def move_enter(price)
+		# abstract
+	end
+
+	def cancel_enter
+		# abstract
+	end
+	
+	def order_list
+		# abstract
+	end
+
+	def position
+		# abstract
+	end
+
+	def deposit_value
+		# abstract
+	end
+
+	protected
+
+	def decode(data)
+		ActiveSupport::JSON.decode(data)
+	end
+
 end
